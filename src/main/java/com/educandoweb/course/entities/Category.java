ddd415c605821a5412a81db1c742da6cf2b696f6
Felipe Jardim <felipeaugustojardimdejesus@gmail.com>
2023-10-11 11:18:24 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -5,24 +5,27 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id //relaciona quem é o Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //mostra que o id será gerado automaticamente identidade
 	private Long id;
 	private String name;
 	
-	@Transient
+	@JsonIgnore //ignorar o Jackson do Json para evitar o looping infinito da tabela
+	@ManyToMany(mappedBy = "categories") // mapeando o ManyToMany associando a categories
 	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
